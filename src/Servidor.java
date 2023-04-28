1131b917fc6f8a2efa298bc12e83fce17b3d2da3
weder.tito <wedertito8@gmail.com>
2023-04-28 15:02:42 -0300

    Refactoring the server

--- src/Servidor.java
+++ src/Servidor.java
@@ -9,11 +9,11 @@ import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-public class Servidor extends Thread{
-    private static ArrayList<BufferedWriter>clientes;
+
+public class Servidor extends Thread {
+
+    private static ArrayList<BufferedWriter> clientes;
     private static ServerSocket server;
     private String nome;
     private Socket con;
@@ -21,77 +21,94 @@ public class Servidor extends Thread{
     private InputStreamReader inr;
     private BufferedReader bfr;
 
-    public Servidor(Socket con){
+    /**
+     * Método construtor
+     * 
+     * @param con do tipo Socket
+     */
+    public Servidor(Socket con) {
         this.con = con;
         try {
-            in  = con.getInputStream();
+            in = con.getInputStream();
             inr = new InputStreamReader(in);
             bfr = new BufferedReader(inr);
         } catch (IOException e) {
+            System.err.println("Erro ao criar streams de entrada: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    public void run(){
-
-        try{
-
+    /**
+     * Método run
+     */
+    public void run() {
+        try {
             String msg;
-            OutputStream ou =  this.con.getOutputStream();
+            OutputStream ou = this.con.getOutputStream();
             Writer ouw = new OutputStreamWriter(ou);
             BufferedWriter bfw = new BufferedWriter(ouw);
             clientes.add(bfw);
             nome = msg = bfr.readLine();
 
-            while(!"Sair".equalsIgnoreCase(msg) && msg != null)
-            {
+            while (!"Sair".equalsIgnoreCase(msg) && msg != null) {
                 msg = bfr.readLine();
                 sendToAll(bfw, msg);
                 System.out.println(msg);
             }
-
-        }catch (Exception e) {
+        } catch (IOException e) {
+            System.err.println("Erro ao enviar mensagem: " + e.getMessage());
             e.printStackTrace();
-
+        } finally {
+            try {
+                con.close();
+            } catch (IOException e) {
+                System.err.println("Erro ao fechar socket: " + e.getMessage());
+                e.printStackTrace();
+            }
         }
     }
 
-    public void sendToAll(BufferedWriter bwSaida, String msg) throws  IOException
-    {
-        BufferedWriter bwS;
+    /***
+     * Método usado para enviar mensagem para todos os clientes conectados
+     * 
+     * @param bwSaida do tipo BufferedWriter
+     * @param msg     do tipo String
+     * @throws IOException
+     */
+    public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
+        BufferedWriter bw;
 
-        for(BufferedWriter bw : clientes){
-            bwS = (BufferedWriter)bw;
-            if(!(bwSaida == bwS)){
-                bw.write(nome + " -> " + msg+"\r\n");
+        for (BufferedWriter cliente : clientes) {
+            bw = (BufferedWriter) cliente;
+            if (!(bwSaida == bw)) {
+                bw.write(nome + " -> " + msg + "\r\n");
                 bw.flush();
             }
         }
     }
 
-    public static void main(String []args) {
+    /***
+     * Método main
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        clientes = new ArrayList<BufferedWriter>();
 
-        try{
-            //Cria os objetos necessário para instânciar o servidor
-            JLabel lblMessage = new JLabel("Porta do Servidor:");
-            JTextField txtPorta = new JTextField("12345");
-            Object[] texts = {lblMessage, txtPorta };
-            JOptionPane.showMessageDialog(null, texts);
-            server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
-            clientes = new ArrayList<BufferedWriter>();
-            JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+
-                    txtPorta.getText());
+        try {
+            int porta = Integer.parseInt(JOptionPane.showInputDialog("Porta do servidor:"));
+            server = new ServerSocket(porta);
+            System.out.println("Servidor ativo na porta: " + porta);
 
-            while(true){
+            while (true) {
                 System.out.println("Aguardando conexão...");
                 Socket con = server.accept();
                 System.out.println("Cliente conectado...");
                 Thread t = new Servidor(con);
                 t.start();
             }
-
-        }catch (Exception e) {
-
+        } catch (IOException e) {
+            System.err.println("Erro ao iniciar servidor: " + e.getMessage());
             e.printStackTrace();
         }
     }
