febc5fbb08fd5d01d323fb7cac22719431f40117
AlessandroG2002 <alessandrogabriel84@gmail.com>
2023-04-29 00:06:40 -0300

    Comentários e organização

--- src/Cliente.java
+++ src/Cliente.java
@@ -15,63 +15,87 @@ import java.net.Socket;
 import javax.swing.*;
 public class Cliente extends JFrame implements ActionListener, KeyListener{
     private static final long serialVersionUID = 1L;
+
+    /* Variáveis para montar ou recebe coisas da interface */
     private JTextArea texto;
     private JTextField txtMsg;
-    private JButton btnSend;
+    private JButton btnEnviar;
     private JButton btnSair;
     private JLabel lblHistorico;
     private JLabel lblMsg;
     private JPanel pnlContent;
+    private JTextField txtIP;
+    private JTextField txtPorta;
+    private JTextField txtNome;
+    /* --- */
+
     private Socket socket;
     private OutputStream ou ;
     private Writer ouw;
     private BufferedWriter bfw;
-    private JTextField txtIP;
-    private JTextField txtPorta;
-    private JTextField txtNome;
 
+    /* Método construtor */
     public Cliente() throws IOException{
+
+        /* Aqui mostrará a tela inicial onde o cliente deve colocar as informações como nome, ip do servidor e porta para prosseguir */
         JLabel lblMessage = new JLabel("Verificar!");
         txtIP = new JTextField("127.0.0.1");
         txtPorta = new JTextField("12345");
         txtNome = new JTextField("Cliente");
         Object[] texts = {lblMessage, txtIP, txtPorta, txtNome };
         JOptionPane.showMessageDialog(null, texts);
-        pnlContent = new JPanel();
-        texto              = new JTextArea(10,20);
-        texto.setEditable(false);
-        texto.setBackground(new Color(240,240,240));
-        txtMsg                       = new JTextField(20);
-        lblHistorico     = new JLabel("Histórico");
-        lblMsg        = new JLabel("Mensagem");
-        btnSend                     = new JButton("Enviar");
-        btnSend.setToolTipText("Enviar Mensagem");
-        btnSair           = new JButton("Sair");
-        btnSair.setToolTipText("Sair do Chat");
-        btnSend.addActionListener(this);
-        btnSair.addActionListener(this);
-        btnSend.addKeyListener(this);
-        txtMsg.addKeyListener(this);
-        JScrollPane scroll = new JScrollPane(texto);
-        texto.setLineWrap(true);
-        pnlContent.add(lblHistorico);
-        pnlContent.add(scroll);
-        pnlContent.add(lblMsg);
-        pnlContent.add(txtMsg);
-        pnlContent.add(btnSair);
-        pnlContent.add(btnSend);
-        pnlContent.setBackground(Color.LIGHT_GRAY);
-        texto.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
-        txtMsg.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
-        setTitle(txtNome.getText());
-        setContentPane(pnlContent);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setSize(250,300);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        /* --- */
+
+        /* Aqui é para construir a janela do chat, iremos dividir em partes para ficar mais fácil a compreensão */
+        pnlContent = new JPanel(); /* Declaração do painel */
+
+            /* Histórico do chat */
+            lblHistorico = new JLabel("Histórico"); /* Título mostrando que é o histórico do chat */
+            texto = new JTextArea(10,20); /* Área onde ficará o texto, definindo largura e altura */
+            texto.setEditable(false); /* Não deixa o texto ser editável, afinal, é só para ver as mensagens que enviou e recebeu */
+            texto.setBackground(new Color(240,240,240)); /* Cor de fundo da área */
+            JScrollPane scroll = new JScrollPane(texto); /* Adicionar um scroll no histórico */
+            texto.setLineWrap(true); /* Quebra de linha */
+            texto.setBorder(BorderFactory.createEtchedBorder(Color.GREEN, Color.GREEN)); /* Borda verde */
+
+            /* Janela de enviar mensagem */
+            lblMsg = new JLabel("Mensagem"); /* Título mostrando que é para enviar mensagens */
+            txtMsg = new JTextField(20); /* Definindo largura, a mesma que a do histórico para ficar padronizado */
+            txtMsg.addKeyListener(this); /* Perceber quando for clicado */
+            txtMsg.setBorder(BorderFactory.createEtchedBorder(Color.GREEN, Color.GREEN)); /* Borda verde */
+            
+            /* Botão sair */
+            btnSair = new JButton("Sair"); /* Texto do botão, onde estará escrito sair */
+            btnSair.setToolTipText("Sair do Chat"); /* Tooltip que aparecerá caso passe o mouse por cima */
+            btnSair.addActionListener(this); /* Perceber quando for clicado */
+
+            /* Botão enviar */
+            btnEnviar = new JButton("Enviar"); /* Texto do botão, onde estará escrito enviar */
+            btnEnviar.setToolTipText("Enviar Mensagem"); /* Tooltip que aparecerá caso passe o mouse por cima */
+            btnEnviar.addActionListener(this);            
+            btnEnviar.addKeyListener(this); /* Perceber quando for clicado */
+            
+            /* Define o content da tela */
+            pnlContent.add(lblHistorico);
+            pnlContent.add(scroll);
+            pnlContent.add(lblMsg);
+            pnlContent.add(txtMsg);
+            pnlContent.add(btnSair);
+            pnlContent.add(btnEnviar);
+            pnlContent.setBackground(Color.LIGHT_GRAY);
+
+            /* Define propriedades da tela */
+            setTitle(txtNome.getText()); /* Título igual ao nome do usuário */
+            setContentPane(pnlContent); /* Conteúdo */
+            setLocationRelativeTo(null); /* Não define posição fixa na tela */
+            setSize(250,350); /* Tamanho fixo em pixels */
+            setResizable(false); /* Não permite mudar o tamanho da tela */
+            setVisible(true); /* Aparecer assim que terminar de gerar */
+            setDefaultCloseOperation(EXIT_ON_CLOSE); /* Finalizar caso clicar no X */
+        /* --- */
     }
 
+    /* Método para conectar-se ao servidor */
     public void conectar() throws IOException{
 
         socket = new Socket(txtIP.getText(),Integer.parseInt(txtPorta.getText()));
@@ -82,19 +106,26 @@ public class Cliente extends JFrame implements ActionListener, KeyListener{
         bfw.flush();
     }
 
+    /* Método para enviar mensagem ao servidor */
     public void enviarMensagem(String msg) throws IOException{
 
         if(msg.equals("Sair")){
             bfw.write("Desconectado \r\n");
             texto.append("Desconectado \r\n");
+            ou.close();
+            ouw.close();
+            bfw.close();
+            socket.close();
+            System.exit(0); /* Tentamos implementar fechar a tela automaticamente caso escreva sair ou clique no botão sair, mas não deu certo */
         }else{
             bfw.write(msg+"\r\n");
-            texto.append( txtNome.getText() + " diz -> " +         txtMsg.getText()+"\r\n");
+            texto.append( txtNome.getText() + " diz -> " + txtMsg.getText()+"\r\n");
         }
         bfw.flush();
         txtMsg.setText("");
     }
 
+    /* Método para receber mensagens e coloca-las no histórico */
     public void escutar() throws IOException{
 
         InputStream in = socket.getInputStream();
@@ -106,37 +137,29 @@ public class Cliente extends JFrame implements ActionListener, KeyListener{
 
             if(bfr.ready()){
                 msg = bfr.readLine();
-                if(msg.equals("Sair"))
-                    texto.append("Servidor caiu! \r\n");
-                else
-                    texto.append(msg+"\r\n");
+                texto.append(msg+"\r\n");
             }
     }
 
-    public void sair() throws IOException{
-
-        enviarMensagem("Sair");
-        bfw.close();
-        ouw.close();
-        ou.close();
-        socket.close();
-    }
-
+    /* Método para definir o que será feito quando os botões forem pressionados */
     @Override
     public void actionPerformed(ActionEvent e) {
 
         try {
-            if(e.getActionCommand().equals(btnSend.getActionCommand()))
+            if(e.getActionCommand().equals(btnEnviar.getActionCommand())) /* Botão de enviar */
                 enviarMensagem(txtMsg.getText());
             else
-            if(e.getActionCommand().equals(btnSair.getActionCommand()))
-                sair();
+            if(e.getActionCommand().equals(btnSair.getActionCommand())) /* Botão de sair */
+            {
+                enviarMensagem("Sair");
+            }
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
     }
 
+    /* Método para definir o que será feito quando apertar enter enquanto na caixa de texto de mensagem */
     @Override
     public void keyPressed(KeyEvent e) {
 
@@ -160,6 +183,7 @@ public class Cliente extends JFrame implements ActionListener, KeyListener{
         // TODO Auto-generated method stub
     }
 
+    /* Método main */
     public static void main(String []args) throws IOException{
 
         Cliente app = new Cliente();
--- src/Servidor.java
+++ src/Servidor.java
@@ -21,46 +21,43 @@ public class Servidor extends Thread {
     private InputStreamReader inr;
     private BufferedReader bfr;
 
-    /**
-     * Método construtor
-     * 
-     * @param con do tipo Socket
-     */
+     /* Método construtor, parâmetro Socket para poder pegar as informações do mesmo */
     public Servidor(Socket con) {
         this.con = con;
         try {
-            in = con.getInputStream();
-            inr = new InputStreamReader(in);
-            bfr = new BufferedReader(inr);
+            in = con.getInputStream(); /* Pegar InputStream */
+            inr = new InputStreamReader(in); /* Pegar InputStreamReader */
+            bfr = new BufferedReader(inr); /* Pegar BufferedRead */
+            /* Isso para poder enviar as mensagens para os clientes no futuro */
         } catch (IOException e) {
             System.err.println("Erro ao criar streams de entrada: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    /**
-     * Método run
-     */
     public void run() {
         try {
-            String msg;
+            /* --- */
+            String msg; 
             OutputStream ou = this.con.getOutputStream();
-            Writer ouw = new OutputStreamWriter(ou);
+            Writer ouw = new OutputStreamWriter(ou); /* O autor da mensagem */
             BufferedWriter bfw = new BufferedWriter(ouw);
-            clientes.add(bfw);
+            clientes.add(bfw); /* Adicionando o escritor da mensagem a lista de clientes */
             nome = msg = bfr.readLine();
+            /* Essa parte é a que recebe as mensagens dos clientes */
+            /* --- */
 
-            while (!"Sair".equalsIgnoreCase(msg) && msg != null) {
+            while (!"Sair".equalsIgnoreCase(msg) && msg != null) { /* Se a mensagem NãO for igual a Sair ou nula, então percorre o while */
                 msg = bfr.readLine();
-                sendToAll(bfw, msg);
-                System.out.println(msg);
+                sendToAll(bfw, msg); /* Método criado para compartilhar a mensagens com os outros clientes conectados */
+                System.out.println(msg); /* Printar no terminal a mensagem */
             }
         } catch (IOException e) {
             System.err.println("Erro ao enviar mensagem: " + e.getMessage());
             e.printStackTrace();
-        } finally {
+        } finally { /* Caso o método run vá finalizar, então... */
             try {
-                con.close();
+                con.close(); /* Feche o Socket, ou seja, a conexão */
             } catch (IOException e) {
                 System.err.println("Erro ao fechar socket: " + e.getMessage());
                 e.printStackTrace();
@@ -68,34 +65,25 @@ public class Servidor extends Thread {
         }
     }
 
-    /***
-     * Método usado para enviar mensagem para todos os clientes conectados
-     * 
-     * @param bwSaida do tipo BufferedWriter
-     * @param msg     do tipo String
-     * @throws IOException
-     */
+     /* Método usado para enviar mensagem para todos os clientes conectados */
     public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
         BufferedWriter bw;
 
-        for (BufferedWriter cliente : clientes) {
+        for (BufferedWriter cliente : clientes) { /* Para todo cliente conectado na lista de clientes... */
             bw = (BufferedWriter) cliente;
-            if (!(bwSaida == bw)) {
-                bw.write(nome + " -> " + msg + "\r\n");
-                bw.flush();
+            if (!(bwSaida == bw)) { /* Se o cliente selecionado da lista não for igual ao autor da mensagem, então replique a mensagem para ele */
+                bw.write(nome + " -> " + msg + "\r\n"); /* Define o texto a ser enviado */
+                bw.flush(); /* Envia o texto */
             }
         }
     }
 
-    /***
-     * Método main
-     * 
-     * @param args
-     */
+     /* Método main */
     public static void main(String[] args) {
-        clientes = new ArrayList<BufferedWriter>();
+        clientes = new ArrayList<BufferedWriter>(); /* Lista de clientes conectados */
 
         try {
+            /* Aqui abre uma janela de input para colocar a porta que o servidor irá utilizar */
             int porta = Integer.parseInt(JOptionPane.showInputDialog("Porta do servidor:"));
             server = new ServerSocket(porta);
             System.out.println("Servidor ativo na porta: " + porta);
@@ -105,7 +93,7 @@ public class Servidor extends Thread {
                 Socket con = server.accept();
                 System.out.println("Cliente conectado...");
                 Thread t = new Servidor(con);
-                t.start();
+                t.start(); /* Basicamente, é uma thread por cliente */
             }
         } catch (IOException e) {
             System.err.println("Erro ao iniciar servidor: " + e.getMessage());
